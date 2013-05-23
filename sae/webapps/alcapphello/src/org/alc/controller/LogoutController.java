ec97d3559e69d90fa69abcf322e4f6302c14ccce
valefor <adrain.f.tepes@gmail.com>
2013-05-24 00:09:51 +0800

    modified:   sae/webapps/alcapphello/WebContent/WEB-INF/zk-label_en.properties 	modified:   sae/webapps/alcapphello/WebContent/WEB-INF/zk-label_zh_CN.properties 	modified:   sae/webapps/alcapphello/WebContent/layout/banner.zul 	modified:   sae/webapps/alcapphello/WebContent/public/login.zul 	modified:   sae/webapps/alcapphello/src/org/alc/controller/LogoutController.java

--- sae/webapps/alcapphello/src/org/alc/controller/LogoutController.java
+++ sae/webapps/alcapphello/src/org/alc/controller/LogoutController.java
@@ -1,6 +1,7 @@
 package org.alc.controller;
 
 import org.alc.util.SecurityUtil;
+import org.zkoss.util.resource.Labels;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.event.Event;
@@ -22,11 +23,7 @@ public class LogoutController extends SelectorComposer<Component> {
 	private static final long serialVersionUID = 1L;
 
 	@Wire
-	Hbox statusBar;
-	
-	Label anonymous = new Label();
-	Label username = new Label();
-	Label logout = new Label();
+	Hbox statusBar;	
 	
 	@Override
 	public void doAfterCompose(Component comp) throws Exception {
@@ -34,14 +31,31 @@ public class LogoutController extends SelectorComposer<Component> {
 	
 		if (SecurityUtil.isAnonymous()) {
 			Label anonymous = new Label();
-			anonymous.setValue("Anonymous");
+			Label login = new Label();
+			anonymous.setValue(Labels.getLabel("app.anonymous"));
+			
+			login.setValue(Labels.getLabel("app.login"));
+			login.setSclass("logout");
+			EventListener<Event> onActionListener = new SerializableEventListener<Event>(){
+	            private static final long serialVersionUID = 1L;
+	 
+	            public void onEvent(Event event) throws Exception {
+	                //redirect current url to logout
+	            	Executions.getCurrent().sendRedirect("/public/login.zul");
+	            }
+	        };
+	        login.addEventListener(Events.ON_CLICK, onActionListener);
+			
 			statusBar.appendChild(anonymous);
+			statusBar.appendChild(login);
 		} else {
 			String name = SecurityUtil.getUser().getUsername();
 			if (name != null) {
+				Label username = new Label();
+				Label logout = new Label();
 				username.setValue(SecurityUtil.getUser().getUsername());
 				
-				logout.setValue("Logout");
+				logout.setValue(Labels.getLabel("app.logout"));
 				logout.setSclass("logout");							
 				EventListener<Event> onActionListener = new SerializableEventListener<Event>(){
 		            private static final long serialVersionUID = 1L;
