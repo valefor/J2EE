08944dbc854f9426562c9d0be82c99daca97947c
Herr_Alucard <adrain.f.tepes@gmail.com>
2013-08-13 00:39:30 +0800

    small commit at 8/13

---
+++ sae/webapps/alcapphello/src/org/alc/controller/MessageBarController.java
@@ -0,0 +1,13 @@
+package org.alc.controller;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.SelectorComposer;
+
+public class MessageBarController extends SelectorComposer<Component> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
