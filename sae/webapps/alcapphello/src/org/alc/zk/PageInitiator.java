1ef4aece03b08863194f9895b13aa5de88cfd641
Herr_Alucard <adrain.f.tepes@gmail.com>
2013-09-16 17:55:21 +0800

    small update

--- sae/webapps/alcapphello/src/org/alc/zk/PageInitiator.java
+++ sae/webapps/alcapphello/src/org/alc/zk/PageInitiator.java
@@ -13,15 +13,15 @@ public class PageInitiator implements Initiator {
 	public void doInit(Page page, Map<String, Object> args) throws Exception {
 		
 		PageImpl pageImpl = (PageImpl)page;
-		
-		// meta
-		pageImpl.addBeforeHeadTags("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>");
-		// css and js
-		pageImpl.addAfterHeadTags("<link rel=\"stylesheet\" type=\"text/css\" href=\"/alcapphello/rui/css/bootstrap.min.css\"/>");
-		pageImpl.addAfterHeadTags("<link rel=\"stylesheet\" type=\"text/css\" href=\"/alcapphello/rui/css/bootstrap-responsive.min.css\"/>");
-	
-		System.out.println(page.getRequestPath());
-		System.out.println(pageImpl.getBeforeHeadTags());
-		System.out.println(pageImpl.getAfterHeadTags());
+//		
+//		// meta
+//		pageImpl.addBeforeHeadTags("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>");
+//		// css and js
+//		pageImpl.addAfterHeadTags("<link rel=\"stylesheet\" type=\"text/css\" href=\"/alcapphello/rui/css/bootstrap.min.css\"/>");
+//		pageImpl.addAfterHeadTags("<link rel=\"stylesheet\" type=\"text/css\" href=\"/alcapphello/rui/css/bootstrap-responsive.min.css\"/>");
+//	
+		System.out.println("Page owner:"+ pageImpl.getOwner());
+//		System.out.println(pageImpl.getBeforeHeadTags());
+//		System.out.println(pageImpl.getAfterHeadTags());
 	}
 }
