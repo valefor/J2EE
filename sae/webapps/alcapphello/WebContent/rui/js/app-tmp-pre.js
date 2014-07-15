96ba69be981410a1c1263a362945ac3fcdb487c7
Herr_Alucard <adrain.f.tepes@gmail.com>
2014-07-15 18:08:28 +0800

    validate reg form

--- sae/webapps/alcapphello/WebContent/rui/js/app-tmp-pre.js
+++ sae/webapps/alcapphello/WebContent/rui/js/app-tmp-pre.js
@@ -1,4 +1,3 @@
-
 !function($){
 /*	
 $('body').css({
--- sae/webapps/alcapphello/WebContent/rui/js/bs-adapt.js
+++ sae/webapps/alcapphello/WebContent/rui/js/bs-adapt.js
@@ -1,3 +1,22 @@
+function bitSet(va, bit) {
+	if (bit < 0) { return false; }
+	return va |= (1 << bit);
+}
+
+function bitReset(va, bit) {
+	if (bit < 0) { return false; }
+	return va &= ~(1 << bit);
+}
+
+function bitFlip(va, bit) {
+	if (bit < 0) { return false; }
+	return va ^= (1 << bit);
+}
+
+function bitIsset(va, bit) {
+	if (bit < 0) { return false; }
+	return (va &= (1 << bit)) != 0;
+}
 zk.afterLoad('zul.wgt', function () {
 	var _button = {};
 
